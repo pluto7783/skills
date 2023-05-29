c04f79e76a4e7ab4a5eec33d0003c647650501bb
DESKTOP-VU8J26H\kjc <karolus@naver.com>
2023-05-29 21:40:41 +0900

    websocket make"

---
+++ karolus/src/test/java/com/websocket/karolus/KarolusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.karolus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarolusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
