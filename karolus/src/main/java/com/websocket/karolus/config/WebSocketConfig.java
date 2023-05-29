b4fa49097de0e97456fc55dddca71545d9b90fac
DESKTOP-VU8J26H\kjc <karolus@naver.com>
2023-05-29 22:53:39 +0900

    websocket sample

---
+++ karolus/src/main/java/com/websocket/karolus/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.websocket.karolus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
